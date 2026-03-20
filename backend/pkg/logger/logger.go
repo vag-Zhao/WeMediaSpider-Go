457914564d35755b9165fb868de32148dcd907be
vag-Zhao <zgs3344@hunnu.edu.cn>
2026-03-20 11:58:18 -0600

    feat(logger): 重写 logger.go，替换 logrus 为 zap

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,146 +1,108 @@
 package logger
 
 import (
-	"io"
 	"os"
 	"path/filepath"
 
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/lumberjack.v2"
 )
 
-var Log *logrus.Logger
+// Log 全局 logger，Init() 后可直接使用
+var (
+	Log         *zap.Logger
+	atomicLevel zap.AtomicLevel
+)
 
-// Init 初始化日志系统
+// Init 初始化日志系统。目录创建失败时降级为仅 Stdout 输出。
 func Init() error {
-	Log = logrus.New()
-
-	// 设置日志格式
-	Log.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05",
-		ForceColors:     false,
-	})
-
-	// 设置日志级别
-	Log.SetLevel(logrus.InfoLevel)
-
-	// 初始化日志缓冲区（保留最近 1000 条）
+	atomicLevel = zap.NewAtomicLevelAt(zapcore.InfoLevel)
 	InitBuffer(1000)
 
-	// TODO: 将在 Task 3 中替换
-	// Log.AddHook(&BufferHook{})
+	consoleEnc := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
+		TimeKey:        "T",
+		LevelKey:       "L",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
+		EncodeDuration: zapcore.StringDurationEncoder,
+	})
 
-	// 创建日志目录
-	homeDir, _ := os.UserHomeDir()
-	logDir := filepath.Join(homeDir, ".wemediaspider", "logs")
-	os.MkdirAll(logDir, 0755)
+	jsonEnc := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
+		EncodeDuration: zapcore.StringDurationEncoder,
+	})
 
-	// 创建日志文件
-	logFile := filepath.Join(logDir, "app.log")
-	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
+	// 控制台输出
+	consoleCore := zapcore.NewCore(consoleEnc, zapcore.AddSync(os.Stdout), atomicLevel)
+
+	// 内存缓冲区输出
+	bufCore := &BufferCore{
+		enc:   consoleEnc.Clone(),
+		buf:   buffer,
+		level: atomicLevel,
 	}
 
-	// 同时输出到文件和控制台
-	multiWriter := io.MultiWriter(os.Stdout, file)
-	Log.SetOutput(multiWriter)
+	cores := []zapcore.Core{consoleCore, bufCore}
 
+	// 文件输出（失败时降级）
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		Log = zap.New(zapcore.NewTee(cores...))
+		Log.Warn("无法获取用户目录，日志文件输出已禁用")
+		return nil
+	}
+
+	logDir := filepath.Join(homeDir, ".wemediaspider", "logs")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		Log = zap.New(zapcore.NewTee(cores...))
+		Log.Warn("无法创建日志目录，日志文件输出已禁用", zap.Error(err))
+		return nil
+	}
+
+	fileWriter := &lumberjack.Logger{
+		Filename:   filepath.Join(logDir, "app.log"),
+		MaxSize:    10,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   true,
+		LocalTime:  true,
+	}
+	fileCore := zapcore.NewCore(jsonEnc, zapcore.AddSync(fileWriter), atomicLevel)
+	cores = append(cores, fileCore)
+
+	Log = zap.New(zapcore.NewTee(cores...))
 	return nil
 }
 
-// TODO: 将在 Task 3 中替换
-// BufferHook 日志缓冲区 Hook
-// type BufferHook struct{}
-//
-// func (h *BufferHook) Levels() []logrus.Level {
-// 	return logrus.AllLevels
-// }
-//
-// func (h *BufferHook) Fire(entry *logrus.Entry) error {
-// 	if buffer != nil {
-// 		logLine := fmt.Sprintf("[%s] %s: %s",
-// 			entry.Time.Format("2006-01-02 15:04:05"),
-// 			entry.Level.String(),
-// 			entry.Message,
-// 		)
-// 		buffer.AddLog(logLine)
-// 	}
-// 	return nil
-// }
-
-// Info 记录信息日志
-func Info(args ...interface{}) {
-	if Log != nil {
-		Log.Info(args...)
-	}
-}
-
-// Infof 记录格式化信息日志
-func Infof(format string, args ...interface{}) {
-	if Log != nil {
-		Log.Infof(format, args...)
-	}
-}
-
-// Warn 记录警告日志
-func Warn(args ...interface{}) {
-	if Log != nil {
-		Log.Warn(args...)
-	}
-}
-
-// Warnf 记录格式化警告日志
-func Warnf(format string, args ...interface{}) {
-	if Log != nil {
-		Log.Warnf(format, args...)
-	}
-}
-
-// Error 记录错误日志
-func Error(args ...interface{}) {
-	if Log != nil {
-		Log.Error(args...)
-	}
-}
-
-// Errorf 记录格式化错误日志
-func Errorf(format string, args ...interface{}) {
-	if Log != nil {
-		Log.Errorf(format, args...)
-	}
-}
-
-// Debug 记录调试日志
-func Debug(args ...interface{}) {
-	if Log != nil {
-		Log.Debug(args...)
-	}
-}
-
-// Debugf 记录格式化调试日志
-func Debugf(format string, args ...interface{}) {
-	if Log != nil {
-		Log.Debugf(format, args...)
-	}
-}
-
-// SetLevel 设置日志级别
+// SetLevel 运行时动态调整日志级别（"debug"|"info"|"warn"|"error"）
 func SetLevel(level string) {
-	if Log == nil {
-		return
-	}
-
 	switch level {
 	case "debug":
-		Log.SetLevel(logrus.DebugLevel)
+		atomicLevel.SetLevel(zapcore.DebugLevel)
 	case "info":
-		Log.SetLevel(logrus.InfoLevel)
+		atomicLevel.SetLevel(zapcore.InfoLevel)
 	case "warn":
-		Log.SetLevel(logrus.WarnLevel)
+		atomicLevel.SetLevel(zapcore.WarnLevel)
 	case "error":
-		Log.SetLevel(logrus.ErrorLevel)
+		atomicLevel.SetLevel(zapcore.ErrorLevel)
 	default:
-		Log.SetLevel(logrus.InfoLevel)
+		atomicLevel.SetLevel(zapcore.InfoLevel)
+	}
+}
+
+// Sync 刷新所有缓冲写入，程序退出前调用
+func Sync() {
+	if Log != nil {
+		_ = Log.Sync()
 	}
 }
